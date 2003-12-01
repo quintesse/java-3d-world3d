ea9f6a6d82dd39828a16c37b426130f066e86693
tako <devnull@localhost>
2003-12-01 23:26:09 +0100

    [svn] Fixed range calculation problem.

--- src/org/codejive/world3d/EntitiesWithinRangeIterator.java
+++ src/org/codejive/world3d/EntitiesWithinRangeIterator.java
@@ -24,7 +24,7 @@ public class EntitiesWithinRangeIterator extends ConditionalIterator {
 	public EntitiesWithinRangeIterator(Iterator _iter, Point3f _center, float _fRange, Class _class, boolean _bMustBeTargetable) {
 		super(_iter);
 		m_center = _center;
-		m_fRangeSquared = _fRange;
+		m_fRangeSquared = _fRange * _fRange;
 		m_class = _class;
 		m_bMustBeTargetable = _bMustBeTargetable;
 	}
@@ -37,9 +37,8 @@ public class EntitiesWithinRangeIterator extends ConditionalIterator {
 		Entity e = (Entity)_element;
 		Point3f pos = e.getPosition();
 		if (m_center.distanceSquared(pos) <= m_fRangeSquared) {
-			if ((m_class == null) || m_class.isInstance(e.getEntityClass())) {
+			if ((m_class == null) || m_class.isInstance(e)) {
 				if (!m_bMustBeTargetable || e.isTargetable()) {
-System.err.println(this + " x " + _element + " dist = " + m_center.distanceSquared(pos));
 					bInclude = true;
 				}
 			}
